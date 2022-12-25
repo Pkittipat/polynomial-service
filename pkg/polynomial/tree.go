85451afa149a2f74202bd65e256f1b56b4659730
Pkittipat <kittipat.phongsak@gmail.com>
2022-12-25 18:28:26 +0700

    remove: unsued lines

--- pkg/polynomial/tree.go
+++ pkg/polynomial/tree.go
@@ -5,19 +5,3 @@ type Node struct {
 	Next  *Node
 	Prev  *Node
 }
-
-// func (n *Node) Insert(value *int) {
-// 	if value < n.Value {
-// 		if n.Left == nil {
-// 			n.Left = &Node{Value: value}
-// 		} else {
-// 			n.Left.Insert(value)
-// 		}
-// 	} else {
-// 		if n.Right == nil {
-// 			n.Right = &Node{Value: value}
-// 		} else {
-// 			n.Right.Insert(value)
-// 		}
-// 	}
-// }
