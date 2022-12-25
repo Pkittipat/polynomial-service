71f8490294a8f8aadbc2905cf8875397c0018616
Pkittipat <kittipat.phongsak@gmail.com>
2022-12-25 18:52:27 +0700

    add: testcase

--- pkg/polynomial/polynomial_test.go
+++ pkg/polynomial/polynomial_test.go
@@ -2,38 +2,7 @@ package polynomial
 
 import "testing"
 
-// import (
-// 	"testing"
-// )
-
-// func TestPolynomial(t *testing.T) {
-// 	testcases := []struct {
-// 		name string
-// 		in   []string
-// 		want []int
-// 	}{
-// 		{
-// 			name: "",
-// 			in:   []string{"1", "x", "8", "17", "y", "z", "78", "113"},
-// 			want: []int{},
-// 		},
-// 	}
-
-// 	for _, tc := range testcases {
-// 		t.Run(tc.name, func(t *testing.T) {
-// 			p := NewPolynomial(tc.in)
-// 			// result := traverseDoublyLinkedList(p.Root)
-// 			findMissingValue(p.Root)
-// 		})
-// 	}
-// }
-
-// func toIntPtr(v int) *int {
-// 	return &v
-// }
-
 func TestFindNextValue(t *testing.T) {
-	// dataset := []string{"1", "x", "8", "17", "y", "z", "78", "113"}
 	polynomial := NewPolynomial()
 	slice := traverseDoublyLinkedList(polynomial.GetRoot())
 	testcases := []struct {
@@ -47,14 +16,6 @@ func TestFindNextValue(t *testing.T) {
 	for _, tc := range testcases {
 		t.Run(tc.name, func(t *testing.T) {
 			got := findNextValue(slice[tc.index])
-			// if tc.want == nil && got != nil {
-			// 	t.Errorf("expected: %v got: %v", tc.want, got)
-			// }
-			// if tc.want != nil && got != nil {
-			// 	if *tc.want != *got {
-			// 		t.Errorf("expected: %v got: %v", tc.want, got)
-			// 	}
-			// }
 			if tc.want != got {
 				t.Errorf("expected: %v got: %v", tc.want, got)
 			}
@@ -62,51 +23,73 @@ func TestFindNextValue(t *testing.T) {
 	}
 }
 
-// func TestFindPrevValue(t *testing.T) {
-// 	dataset := []string{"1", "x", "8", "17", "y", "z", "78", "113"}
-// 	polynomial := NewPolynomial(dataset)
-// 	slice := traverseDoublyLinkedList(polynomial.Root)
-// 	testcases := []struct {
-// 		name  string
-// 		index int
-// 		want  *int
-// 	}{
-// 		{name: "", index: 1, want: toIntPtr(1)},
-// 		{name: "", index: 4, want: toIntPtr(17)},
-// 	}
-// 	for _, tc := range testcases {
-// 		t.Run(tc.name, func(t *testing.T) {
-// 			got := findPrevValue(slice[tc.index])
-// 			if tc.want == nil && got != nil {
-// 				t.Errorf("expected: %v got: %v", tc.want, got)
-// 			}
-// 			if tc.want != nil && got != nil {
-// 				if *tc.want != *got {
-// 					t.Errorf("expected: %v got: %v", tc.want, got)
-// 				}
-// 			}
-// 		})
-// 	}
-// }
+func TestFindPrevValue(t *testing.T) {
+	polynomial := NewPolynomial()
+	slice := traverseDoublyLinkedList(polynomial.GetRoot())
+	testcases := []struct {
+		name  string
+		index int
+		want  int
+	}{
+		{name: "index_one", index: 1, want: 1},
+		{name: "index_four", index: 4, want: 17},
+	}
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			got := findPrevValue(slice[tc.index])
+			if tc.want != got {
+				t.Errorf("expected: %v got: %v", tc.want, got)
+			}
+		})
+	}
+}
 
-// func TestFindDifferenceNumber(t *testing.T) {
-// 	testcases := []struct {
-// 		name string
-// 		arg  []int
-// 		want []int
-// 	}{
-// 		{
-// 			name: "",
-// 			arg:  []int{1, 3, 8, 17, 31, 51, 78, 113},
-// 			want: []int{1, 1, 1, 1, 1},
-// 		},
-// 	}
-// 	for _, tc := range testcases {
-// 		t.Run(tc.name, func(t *testing.T) {
-// 			got := findDifferenceNumber(tc.arg, 0)
-// 			if len(tc.want) != len(got) {
-// 				t.Errorf("expected %v got %v", len(tc.want), len(got))
-// 			}
-// 		})
-// 	}
-// }
+func TestFindDifferenceNumber(t *testing.T) {
+	testcases := []struct {
+		name string
+		arg  []int
+		want []int
+	}{
+		{
+			name: "case_polynomial_is_one",
+			arg:  []int{1, 3, 8, 17, 31, 51, 78, 113},
+			want: []int{1, 1, 1, 1, 1},
+		},
+	}
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			got := findDifferenceNumber(tc.arg, 0)
+			if len(tc.want) != len(got) {
+				t.Errorf("expected %v got %v", len(tc.want), len(got))
+			}
+		})
+	}
+}
+
+func TestIsPolynomial(t *testing.T) {
+	testcases := []struct {
+		name string
+		arg  []int
+		want bool
+	}{
+		{
+			name: "case_not_related",
+			arg:  []int{1, 2, 1, 1},
+			want: false,
+		},
+		{
+			name: "case_related",
+			arg:  []int{1, 1, 1, 1},
+			want: true,
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			got := isPolynomial(tc.arg)
+			if tc.want != got {
+				t.Errorf("expexted: %v, accepted: %v", tc.want, got)
+			}
+		})
+	}
+}
